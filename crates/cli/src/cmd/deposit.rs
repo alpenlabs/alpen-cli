alpenlabs/alpen

fix(alpen-cli): recover full signet balance after wallet reset (#2177)
* fix(alpen-cli): raise stop gap for signet wallet full scans

A full scan stopped after 5 unused addresses in a row. Most
wallets, including Bitcoin Core, use a stop gap of 20 addresses.
A low stop gap can end a scan before it reaches funds
sitting past a short run of unused addresses.

Raise the stop gap to 20 and name both scan constants for
clarity.

* fix(alpen-cli): sync triggers a full scan for unrevealed wallets

A wallet freshly restored from seed has no revealed addresses on
either keychain. The sync command only rechecks addresses that are
already revealed. As a result, it never finds funds at other
indices.

Make sync run a full scan when neither keychain has a revealed
address, then persist the result as before. Add needs_full_scan
with unit tests.

* fix(alpen-cli): track full scan completion as persisted state

Two problems came from inferring scan state off revealed addresses.
A wallet with no transaction history never reveals an address during
a scan. Without a fix, it would run a full scan on every sync,
forever. Separately, `faucet` with no address argument reveals an
address without ever syncing. This could silently mark a freshly
restored wallet as already scanned, so later commands would miss
real funds sitting at higher indices.

Track full scan completion as its own flag in the wallet's sqlite
database, independent of revealed addresses. `sync` and `scan` set
this flag once a scan finishes. Persist wallet data before setting
the flag, so a crash between the two steps causes a harmless
re-scan next time, not a silent skip.

Add tests for the new persistence functions.

* fix(alpen-cli): persist revealed addresses before broadcasting

`send`, `deposit`, and `recover` each reveal a new address while
building a transaction. None of the three then call persist. Each
CLI command runs in its own process. The reveal lived only in
memory and was lost when the command exited.

A lat

bin/alpen-cli/src/cmd/deposit.rs
@@ -207,6 +207,9 @@ pub async fn deposit(
     )?;
     println!("Built transaction");
 
+    l1w.persist()
+        .internal_error("Failed to persist signet wallet")?;
+
     let pb = ProgressBar::new_spinner().with_message("Saving output descriptor");
     pb.enable_steady_tick(Duration::from_millis(100));
 

bin/alpen-cli/src/cmd/send.rs
@@ -86,6 +86,8 @@ pub async fn send(args: SendArgs, seed: Seed, settings: Settings) -> Result<(), 
                     Err(e) => panic!("Unexpected error in creating PSBT: {e:?}"),
                 }
             };
+            l1w.persist()
+                .internal_error("Failed to persist signet wallet")?;
             l1w.sign(&mut psbt, Default::default())
                 .expect("tx should be signed");
             let tx = psbt.extract_tx().expect("tx should be signed and ready");

alpenlabs/alpen-cli

(cherry picked from commit 22d5ea5fff1bf0e4ea1314105545151233353574)

crates/cli/src/cmd/deposit.rs
@@ -232,6 +232,9 @@ pub async fn deposit(
     )?;
     println!("Built transaction");
 
+    l1w.persist()
+        .internal_error("Failed to persist Bitcoin wallet")?;
+
     let pb = ProgressBar::new_spinner().with_message("Saving output descriptor");
     pb.enable_steady_tick(Duration::from_millis(100));
 

crates/cli/src/cmd/send.rs
@@ -86,6 +86,8 @@ pub async fn send(args: SendArgs, seed: Seed, settings: Settings) -> Result<(), 
                     Err(e) => panic!("Unexpected error in creating PSBT: {e:?}"),
                 }
             };
+            l1w.persist()
+                .internal_error("Failed to persist Bitcoin wallet")?;
             l1w.sign(&mut psbt, Default::default())
                 .expect("tx should be signed");
             let tx = psbt.extract_tx().expect("tx should be signed and ready");